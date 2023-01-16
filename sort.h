470cdf65a1966a3c1794c6246a2e428b66a54568
Ch@rlem@gne <agbolossouc@gmail.com>
2023-01-16 07:52:58 -0800

    bubble sort

--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,3 +18,4 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+#endif
